90716b55309dae407b9e38173fc61f656e3c4d50
CongDuy <cdmaster1999@gmail.com>
2020-06-21 16:29:01 +0700

    following is dune

---
+++ CongDuy_Lab456/Controllers/FollowingsController.cs
@@ -0,0 +1,37 @@
+﻿using CongDuy_Lab456.DTOs;
+using CongDuy_Lab456.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CongDuy_Lab456.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public FollowingsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var UserId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == UserId && f.FolloweeId == followingDto.FolloweeId))
+                return BadRequest("Following already exists!");
+            var folowing = new Following
+            {
+                FollowerId = UserId,
+                FolloweeId = followingDto.FolloweeId
+            };
+            _dbContext.Followings.Add(folowing);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- CongDuy_Lab456/Models/ApplicationDbContext.cs
+++ CongDuy_Lab456/Models/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace CongDuy_Lab456.Models
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -26,6 +27,16 @@ namespace CongDuy_Lab456.Models
             modelBuilder.Entity<Attendance>()
                 .HasRequired(a => a.course)
                 .WithMany()
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
                 .WillCascadeOnDelete(false);
             base.OnModelCreating(modelBuilder);
         }
